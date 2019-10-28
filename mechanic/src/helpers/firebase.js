f543323ed1c23573a8bd779d5e8b3195f408003c
Rogelio <rogercp1234@yahoo.com>
2019-10-28 13:23:56 -0700

    added firebase config

---
+++ mechanic/src/helpers/firebase.js
@@ -0,0 +1,14 @@
+const firebase = require('firebase');
+const firebaseui = require('firebaseui');
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBz6Bol0rPrG-wyJ0MJFFdc7FUoAIgt55I",
+    authDomain: "mech-3223c.firebaseapp.com",
+    databaseURL: "https://mech-3223c.firebaseio.com",
+    projectId: "mech-3223c",
+    storageBucket: "mech-3223c.appspot.com",
+    messagingSenderId: "993844135178",
+    appId: "1:993844135178:web:41f005098762ccb5a366f3",
+    measurementId: "G-0RF7LZM065"
+  };
