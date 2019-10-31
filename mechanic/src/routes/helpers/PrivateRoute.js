faa634820ac37cd18d829073878dd41461bd4cb1
Rogelio <rogercp1234@yahoo.com>
2019-10-31 14:54:28 -0700

    brought in onAuthStateChanged method in private route

--- mechanic/src/routes/helpers/PrivateRoute.js
+++ mechanic/src/routes/helpers/PrivateRoute.js
@@ -2,3 +2,39 @@
  * Dependencies
  */
 
+import React from 'react';
+import { Route } from 'react-router-dom';
+import firebase from 'firebase';
+import uuid from 'uuid';
+import axios from 'axios';
+
+/**
+ * Define route component
+ */
+const PrivateRoute = ({ component: Component, errorBoundary: ErrorBoundary, path, exact }) => {
+
+    firebase.auth().onAuthStateChanged(async (user)=>{
+
+        if(user){
+            
+    
+            let token = await user.getIdToken()
+
+            localStorage.setItem('token',token)
+
+            axios.post(`${process.env.REACT_APP_API_URL}/users/auth`,{
+                user:user,
+                token:token
+            }).then(res =>{
+                localStorage.setItem('id',res.data.id)
+            }).catch(err =>{
+                console.log(err)
+            });
+
+        }
+
+
+
+    })
+
+}
