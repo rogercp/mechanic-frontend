18013192d1ddd20c2575dce3f3232d07eb42226b
Rogelio <rogercp1234@yahoo.com>
2019-11-05 19:26:47 -0800

    created auth callback to home

--- mechanic/src/routes/loggedInRoutes.js
+++ mechanic/src/routes/loggedInRoutes.js
@@ -7,6 +7,7 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { PrivateRoute } from './helpers/index';
 import {
+  Home,
   ErrorBoundary,
   } from '../views/index';
 
@@ -15,9 +16,9 @@ import {
  */
 
 const UsersRouter = [
-    // <PrivateRoute key={uuid.v4()} exact path='/home'
-    // component={}
-    // errorBoundary={ErrorBoundary}/>
+    <PrivateRoute key={uuid.v4()} exact path='/home'
+    component={Home}
+    errorBoundary={ErrorBoundary}/>
   
 ];
 
