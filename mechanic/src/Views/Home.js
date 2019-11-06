f738900ea33d4edee8fe7abf02d5a3643e1fc3e5
Rogelio <rogercp1234@yahoo.com>
2019-11-05 19:29:18 -0800

    added home view for users

---
+++ mechanic/src/Views/Home.js
@@ -0,0 +1,34 @@
+/**
+ * Dependencies
+ */
+
+import React from 'react';
+import Button from "react-bootstrap/Button";
+
+/**
+ * Import styles
+ */
+
+
+
+/**
+ * Define component
+ */
+
+function Home(props) {
+
+
+    return (
+
+        <>
+        <h1>Home</h1>
+      <Button>Home</Button>
+        </>
+       )
+}
+
+/**
+ * Export component
+ */
+
+export default Home;
