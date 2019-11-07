94225779fce3b13e27b0e2ca8cb9522bc17eb049
Rogelio <rogercp1234@yahoo.com>
2019-11-06 22:09:12 -0800

    added debug statements

--- mechanic/src/routes/UsersRouter.js
+++ mechanic/src/routes/UsersRouter.js
@@ -19,7 +19,7 @@ const Routes = [
   <Route key={uuid.v4()} exact path='/login' render={props => <Login {...props} />}/>,
   <Route key={uuid.v4()} exact path='/register' render={props => <Login {...props} />}/>,
   <Route key={uuid.v4()} exact path='/landing' render={props => <Landing {...props} />}/>,
-  <PrivateRoute key={uuid.v4()} exact path='/auth/callback' component={AuthCallBack}/>
+  <PrivateRoute key={uuid.v4()}  path='/auth/callback' component={AuthCallBack} errorBoundary={ErrorBoundary}/>
 ];
 
 /**
