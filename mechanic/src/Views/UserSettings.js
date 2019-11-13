2df18c9d682fd3b132bac3a495fa1af638bb0f3f
Rogelio <rogercp1234@yahoo.com>
2019-11-12 18:37:45 -0800

    created settings view

---
+++ mechanic/src/Views/UserSettings.js
@@ -0,0 +1,32 @@
+/**
+ * Dependencies
+ */
+
+import React from 'react';
+import Button from "react-bootstrap/Button";
+
+/**
+ * Import styles
+ */
+
+
+
+/**
+ * Define component
+ */
+
+function UserSettings(props) {
+
+
+    return (
+        <>
+        <h1>Settings</h1>
+        </>
+       )
+}
+
+/**
+ * Export component
+ */
+
+export default UserSettings;
