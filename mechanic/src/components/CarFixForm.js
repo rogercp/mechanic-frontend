00dab90fb7ee354bd1f155e7383bd4cd376c19bf
Rogelio <rogercp1234@yahoo.com>
2019-11-14 15:56:09 -0800

    created car fix form Component

---
+++ mechanic/src/components/CarFixForm.js
@@ -0,0 +1,34 @@
+/**
+ * Dependencies
+ */
+
+import React from 'react';
+
+/**
+ *  Import styles
+ */
+
+
+
+/**
+ * Define component
+ */
+
+function CarFixForm(props) {
+
+  
+
+    return (
+      <>
+        
+        <h1>My Cars</h1>
+
+      </>
+    );
+};
+
+/**
+ * Export component
+ */
+
+export default CarFixForm;
--- mechanic/src/components/index.js
+++ mechanic/src/components/index.js
@@ -5,3 +5,4 @@ export { default as NavBar } from './NavBar';
 export { default as CarForm } from './CarForm';
 export { default as CarShow } from './CarShow';
 export { default as CarMaintenenceForm } from './CarMaintenenceForm';
+export { default as CarFixForm } from './CarFixForm';
