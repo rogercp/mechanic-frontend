7cd43794fb9feb07e5d57a762e7c8b1e384a26bd
Rogelio <rogercp1234@yahoo.com>
2019-11-13 20:33:13 -0800

    created car show component

---
+++ mechanic/src/components/CarShow.js
@@ -0,0 +1,34 @@
+/**
+ * Dependencies
+ */
+
+import React from 'react';
+
+/**
+ *  Import styles
+ */
+
+import  '../styles/navbar.scss'
+
+/**
+ * Define component
+ */
+
+function CarShow(props) {
+
+  
+
+    return (
+      <>
+        
+        <h1>car forms</h1>
+
+      </>
+    );
+};
+
+/**
+ * Export component
+ */
+
+export default CarShow;
