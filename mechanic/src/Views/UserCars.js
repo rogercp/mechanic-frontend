1bb4657c744d4e148dcf039df755bf8aa2c59889
Rogelio <rogercp1234@yahoo.com>
2019-11-13 18:00:45 -0800

    created grid for car view

--- mechanic/src/Views/UserCars.js
+++ mechanic/src/Views/UserCars.js
@@ -1,11 +1,20 @@
 
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 
 /**
  * Import styles
  */
 
+
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    
+  }));
 
 
 /**
@@ -14,18 +23,18 @@ import Grid from '@material-ui/core/Grid';
 
 function UserSettings(props) {
 
-
+    const classes = useStyles();
     return (
         <>
         <Grid container spacing={0}>
-        <Grid item xs={6}>
-        <div style={{backgroundColor:"red"}}>
+        <Grid item xs={12} sm={6} >
+        <div style={{backgroundColor:"black",height:"100vh"}}>
                 ok
         </div>
         </Grid>
         
-        <Grid item xs={3}>
-        <div style={{backgroundColor:"red"}}>
+        <Grid item xs={12} sm={6} >
+        <div style={{backgroundColor:"orange",height:"100vh"}}>
             hello
         </div>
 
