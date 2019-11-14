cfaf755c8e09b6d49e266f46e35ed4d2c0da0505
Rogelio <rogercp1234@yahoo.com>
2019-11-13 18:11:18 -0800

    created car form component

--- mechanic/src/Views/UserCars.js
+++ mechanic/src/Views/UserCars.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
+import CarForm from '../components/CarForm'
+
 
 /**
  * Import styles
@@ -29,7 +31,7 @@ function UserSettings(props) {
         <Grid container spacing={0}>
         <Grid item xs={12} sm={6} >
         <div style={{backgroundColor:"black",height:"100vh"}}>
-                ok
+                <CarForm/>
         </div>
         </Grid>
         
--- mechanic/src/components/index.js
+++ mechanic/src/components/index.js
@@ -2,3 +2,4 @@
  * Export components
  */
 export { default as NavBar } from './NavBar';
+export { default as CarForm } from './CarForm';
