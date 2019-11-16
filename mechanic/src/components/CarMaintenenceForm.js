41ef2b8cbca20fee050ec9cfda32bf1f0b1c87ad
Rogelio <rogercp1234@yahoo.com>
2019-11-16 15:20:20 -0800

    somewhat organized the form for maintence

--- mechanic/src/components/CarMaintenenceForm.js
+++ mechanic/src/components/CarMaintenenceForm.js
@@ -11,26 +11,78 @@ import {
   KeyboardTimePicker,
   KeyboardDatePicker,
 } from '@material-ui/pickers';
+import Button from '@material-ui/core/Button';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import Input from '@material-ui/core/Input';
+import FilledInput from '@material-ui/core/FilledInput';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+
 /**
  *  Import styles
  */
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  margin: {
+    margin: theme.spacing(1),
 
+  },
+  withoutLabel: {
+    marginTop: theme.spacing(3),
+  },
+  textField: {
+    width: 200,
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+}));
 
 /**
  * Define component
  */
 
 function CarMaintenenceForm(props) {
+  const classes = useStyles();
   const [state, setState] = React.useState({
     checkedA: true,
     checkedB: true,
   });
+  const [values, setValues] = React.useState({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
+  });
 
-  const handleChange = name => event => {
-    setState({ ...state, [name]: event.target.checked });
+  // const handleChange = name => event => {
+  //   setState({ ...state, [name]: event.target.checked });
+  // };
+
+  const handleChange = prop => event => {
+    setValues({ ...values, [prop]: event.target.value });
   };
-  
+
   const [selectedDate, setSelectedDate] = React.useState(new Date());
 
   const handleDateChange = date => {
@@ -39,16 +91,48 @@ function CarMaintenenceForm(props) {
 
     return (
       <>
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <h1>Car CarMaintenence Form</h1>
-
-        <Switch
+      <h1>Car CarMaintenence Form</h1>
+       <div className={classes.root}> 
+       
+      <form className={classes.container} noValidate autoComplete="off"> 
+    
+      <FormControl fullWidth className={classes.margin}>
+      <TextField
+          id="outlined-textarea"
+          label="Multiline Placeholder"
+          placeholder="Placeholder"
+          multiline
+          className={classes.textField}
+          margin="normal"
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-multiline-static"
+          label="Multiline"
+          multiline
+          rows="4"
+          defaultValue="Default Value"
+          className={classes.textField}
+          margin="normal"
+          variant="outlined"
+        />
+          <Switch
         defaultChecked
         value="checkedF"
         color="default"
         inputProps={{ 'aria-label': 'checkbox with default color' }}
         />
 
+      <TextField
+          label="Total Cost"
+          id="standard-start-adornment"
+          className={clsx(classes.margin, classes.textField)}
+          InputProps={{
+            startAdornment: <InputAdornment position="start">$</InputAdornment>,
+          }}
+        />
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      
         <KeyboardDatePicker
           margin="normal"
           id="date-picker-dialog"
@@ -61,6 +145,29 @@ function CarMaintenenceForm(props) {
             }}
             />
           </MuiPickersUtilsProvider>
+
+          <Button
+        variant="contained"
+        color="default"
+        className={classes.button}
+        startIcon={<CloudUploadIcon />}
+        >
+        documents
+        </Button>
+
+          
+        <Button
+        variant="contained"
+        color="primary"
+        size="large"
+        className={classes.button}
+        >
+        Enter
+        </Button>
+         </FormControl>
+         
+     </form> 
+     </div> 
         </>
     );
 };
