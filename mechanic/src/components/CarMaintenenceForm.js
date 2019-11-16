7cf56c2fc12f59dabc316068db64bfb71ccbe338
Rogelio <rogercp1234@yahoo.com>
2019-11-16 14:31:07 -0800

    imported date ui and radio button for car fix

--- mechanic/src/components/CarMaintenenceForm.js
+++ mechanic/src/components/CarMaintenenceForm.js
@@ -3,7 +3,14 @@
  */
 
 import React from 'react';
-
+import Switch from '@material-ui/core/Switch';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
 /**
  *  Import styles
  */
@@ -15,17 +22,46 @@ import React from 'react';
  */
 
 function CarMaintenenceForm(props) {
+  const [state, setState] = React.useState({
+    checkedA: true,
+    checkedB: true,
+  });
 
+  const handleChange = name => event => {
+    setState({ ...state, [name]: event.target.checked });
+  };
   
+  const [selectedDate, setSelectedDate] = React.useState(new Date());
+
+  const handleDateChange = date => {
+    setSelectedDate(date);
+  };
 
     return (
       <>
-        
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
         <h1>Car CarMaintenence Form</h1>
 
-        
+        <Switch
+        defaultChecked
+        value="checkedF"
+        color="default"
+        inputProps={{ 'aria-label': 'checkbox with default color' }}
+        />
 
-      </>
+        <KeyboardDatePicker
+          margin="normal"
+          id="date-picker-dialog"
+          label="Task Completed"
+          format="MM/dd/yyyy"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+              'aria-label': 'change date',
+            }}
+            />
+          </MuiPickersUtilsProvider>
+        </>
     );
 };
 
