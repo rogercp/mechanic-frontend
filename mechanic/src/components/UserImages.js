8909c00e598a737de248376b46a25034752a7ad2
Rogelio <rogercp1234@yahoo.com>
2019-11-18 19:38:22 -0800

    created UserImages component for uploads

---
+++ mechanic/src/components/UserImages.js
@@ -0,0 +1,35 @@
+/**
+ * Dependencies
+ */
+
+import React from 'react';
+import { imagesRef } from '../helpers/firebase';
+
+
+/**
+ *  Import styles
+ */
+
+
+
+/**
+ * Define component
+ */
+
+function UserImages(props) {
+
+
+    return (
+      <>
+        
+        
+
+      </>
+    );
+};
+
+/**
+ * Export component
+ */
+
+export default UserImages;
