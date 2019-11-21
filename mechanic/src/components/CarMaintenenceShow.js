87f7b6e6cd454450ce91ec40e6f3bf9c1c335c31
Rogelio <rogercp1234@yahoo.com>
2019-11-20 19:38:56 -0800

    car maintenece record show

---
+++ mechanic/src/components/CarMaintenenceShow.js
@@ -0,0 +1,36 @@
+/**
+ * Dependencies
+ */
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+/**
+ *  Import styles
+ */
+
+import  '../styles/navbar.scss'
+
+
+/**
+ * Define component
+ */
+
+
+function CarMaintenceShow(props) {
+  
+
+
+    return (
+      <>
+        
+      <h1>MaintenenceShow</h1>
+      </>
+    );
+};
+
+/**
+ * Export component
+ */
+
+export default CarMaintenceShow;
