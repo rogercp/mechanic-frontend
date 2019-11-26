fe463c6121bde32712952707e4ea2428ce240f38
Rogelio <rogercp1234@yahoo.com>
2019-11-25 16:59:35 -0800

    created Carfix card and car maintence show components both working as they should for now

---
+++ mechanic/src/components/CarFixCard.js
@@ -0,0 +1,39 @@
+/**
+ * Dependencies
+ */
+
+import React, { useState, useEffect} from "react";
+
+/**
+ *  Import styles
+ */
+
+import  '../styles/navbar.scss'
+
+
+/**
+ * Define component
+ */
+
+
+function CarFixCard(props) {
+
+   
+    return (
+      <>
+      
+        {props.carFix.id}///
+      </>
+    );
+};
+
+/**
+ * Export component
+ */
+
+export default CarFixCard;
+
+
+
+
+
