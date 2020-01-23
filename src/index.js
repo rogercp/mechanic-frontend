25f3dff52489e36d1863bfebfad09a70d3807acc
Rogelio <rogercp1234@yahoo.com>
2020-01-23 09:37:37 -0800

    time to add redux created a store

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore,applyMiddleware } from 'redux';
+// import { rootReducer } from './components/reducers';
+import thunk from 'redux-thunk';
+
+
 
 // // Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(rootReducer,applyMiddleware(thunk))
+
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>
+, 
+document.getElementById('root'));
